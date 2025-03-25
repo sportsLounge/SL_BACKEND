1d0635787e587eea159de5f2a078a749d42b5f41
JUMTBOX <yjey12@naver.com>
2025-03-25 16:58:11 +0900

    feat. test2

--- src/main/java/com/sportslounge/move/flowtest/RestTestController.java
+++ src/main/java/com/sportslounge/move/flowtest/RestTestController.java
@@ -1,5 +1,8 @@
 package com.sportslounge.move.flowtest;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RestTestController {
 	@GetMapping("/test")
-  	public String tMethod() throws Exception {
-	  return "test success";
+  	public ResponseEntity<Map<String, String>> tMethod() throws Exception {
+	  Map<String, String> response = new HashMap<>();
+	  response.put("result","test_success");
+	  return ResponseEntity.ok(response);
 	}
 }
