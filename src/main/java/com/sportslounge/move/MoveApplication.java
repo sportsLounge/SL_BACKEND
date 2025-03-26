202c89809e06afb828fa86612b8e01ad18c3865b
추현중 <hjchoo19940327@gmail.com>
2025-03-26 09:48:59 +0900

    테스트 주석 및 파일 삭제

--- src/main/java/com/sportslounge/move/MoveApplication.java
+++ src/main/java/com/sportslounge/move/MoveApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoveApplication {
 
-	//test choo
 	public static void main(String[] args) {
 		SpringApplication.run(MoveApplication.class, args);
 	}
