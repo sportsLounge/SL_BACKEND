5b95ac2e551a65ef3595eb06d3154d1a84d543d6
JUMTBOX <yjey12@naver.com>
2025-03-27 01:29:23 +0900

    fix. 환경 변수 로그 '정상화'

--- src/main/java/com/sportslounge/move/MoveApplication.java
+++ src/main/java/com/sportslounge/move/MoveApplication.java
@@ -1,31 +1,12 @@
 package com.sportslounge.move;
 
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class MoveApplication {
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(MoveApplication.class);
-
-		application.addListeners(
-			(ApplicationListener<ApplicationEnvironmentPreparedEvent>) event -> {
-			  ConfigurableEnvironment env = event.getEnvironment();
-
-			  env.getSystemEnvironment().forEach((key,val)
-				  -> Logger.getAnonymousLogger().log(new LogRecord(Level.INFO,"ENV_VAR  >>> KEY:" + key + "VAL:" + val)));
-			});
-
-		application.run(args);
-		//		SpringApplication.run(MoveApplication.class, args);
+	  SpringApplication.run(MoveApplication.class, args);
 	}
 
 }
