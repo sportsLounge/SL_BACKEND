182a730af0ec30035c961834903278ee64424d56
p195472 <yjey12@naver.com>
2025-04-28 15:51:04 +0900

    task. Swagger-ui HTML generation succeed 1. supabase_database 세션 pooler 연결에서 transaction pooler 연결로 변경 2. swagger annotation 관련 lib 설정 3. swagger-ui generation 을 위한 swagger annotation 적용

--- src/main/java/com/sportslounge/move/flowtest/ConnectionTestRepository.java
+++ src/main/java/com/sportslounge/move/flowtest/ConnectionTestRepository.java
@@ -1,8 +1,7 @@
 package com.sportslounge.move.flowtest;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConnectionTestRepository extends JpaRepository<ConnectionTestEntity,String> {
-  List<ConnectionTestEntity> findAll ();
+    void deleteByName(String name);
 }
